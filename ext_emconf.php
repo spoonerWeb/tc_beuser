eec5a921c67bb7c698a6d4a0102bf24b55bf6863
Ivan Kartolo <ivan.kartolo@dkd.de>
2012-01-27 23:06:37 +0000

    import TER 1.0.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tc_beuser/trunk@56857 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,35 +1,37 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "tc_beuser"
+# Extension Manager/Repository config file for ext "tc_beuser".
 #
-# Auto generated 04-05-2007 14:34
+# Auto generated 27-01-2012 23:56
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'TC BE User Admin',
 	'description' => 'A Module for administer BE users more comfortably',
 	'category' => 'module',
-	'author' => 'Ingo Renner',
-	'author_email' => 'ingo.renner@dkd.de',
-	'shy' => '',
+	'shy' => 0,
+	'version' => '1.0.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => 'mod1,mod2,mod3,mod4,mod5',
 	'state' => 'stable',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
+	'author' => 'Ingo Renner',
+	'author_email' => 'ingo.renner@dkd.de',
 	'author_company' => '',
-	'version' => '1.0.3',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,10 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'scm_revision' => '$Id$',
-	'_md5_values_when_last_written' => '',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:47:{s:9:"ChangeLog";s:4:"2a35";s:28:"class.tx_tcbeuser_access.php";s:4:"0f30";s:28:"class.tx_tcbeuser_config.php";s:4:"eb72";s:30:"class.tx_tcbeuser_editform.php";s:4:"5e89";s:31:"class.tx_tcbeuser_grouptree.php";s:4:"5d3e";s:27:"class.tx_tcbeuser_hooks.php";s:4:"5b76";s:30:"class.tx_tcbeuser_overview.php";s:4:"6ba5";s:32:"class.tx_tcbeuser_pwd_wizard.php";s:4:"6ed0";s:32:"class.tx_tcbeuser_recordlist.php";s:4:"0be6";s:21:"ext_conf_template.txt";s:4:"4fb7";s:12:"ext_icon.gif";s:4:"080d";s:17:"ext_localconf.php";s:4:"7e0c";s:14:"ext_tables.php";s:4:"ee65";s:14:"doc/manual.sxw";s:4:"c3ae";s:19:"doc/wizard_form.dat";s:4:"bab3";s:20:"doc/wizard_form.html";s:4:"9121";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"663d";s:22:"mod1/locallang_mod.xml";s:4:"647a";s:19:"mod1/moduleicon.gif";s:4:"68a6";s:14:"mod2/clear.gif";s:4:"cc11";s:13:"mod2/conf.php";s:4:"b8ec";s:14:"mod2/index.php";s:4:"90df";s:18:"mod2/locallang.xml";s:4:"d5e4";s:22:"mod2/locallang_mod.xml";s:4:"ac9e";s:19:"mod2/moduleicon.gif";s:4:"49a3";s:14:"mod2/pwdgen.js";s:4:"6156";s:14:"mod3/clear.gif";s:4:"cc11";s:13:"mod3/conf.php";s:4:"fa59";s:14:"mod3/index.php";s:4:"bc60";s:18:"mod3/locallang.xml";s:4:"10f6";s:22:"mod3/locallang_mod.xml";s:4:"779d";s:19:"mod3/moduleicon.gif";s:4:"9c7c";s:12:"mod4/ajax.js";s:4:"d664";s:14:"mod4/clear.gif";s:4:"cc11";s:13:"mod4/conf.php";s:4:"ec5c";s:14:"mod4/index.php";s:4:"f8cd";s:18:"mod4/locallang.xml";s:4:"0ef5";s:22:"mod4/locallang_mod.xml";s:4:"4193";s:19:"mod4/moduleicon.gif";s:4:"080d";s:17:"mod4/prototype.js";s:4:"725c";s:14:"mod5/clear.gif";s:4:"cc11";s:13:"mod5/conf.php";s:4:"e1c2";s:14:"mod5/index.php";s:4:"dbc0";s:18:"mod5/locallang.xml";s:4:"23d0";s:22:"mod5/locallang_mod.xml";s:4:"4bf7";s:19:"mod5/moduleicon.gif";s:4:"a38a";}',
 );
 
 ?>
