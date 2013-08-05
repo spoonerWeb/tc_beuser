94cd9c880053c216a2a0fbcaf8965e915244b606
Ivan Kartolo <ivan.kartolo@dkd.de>
2013-08-05 07:55:12 +0000

    Change author name and author email. Set TYPO3 version Set extension version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tc_beuser/trunk@78826 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "tc_beuser".
-#
-# Auto generated 28-01-2012 00:20
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tc_beuser".
+ *
+ * Auto generated 05-08-2013 09:54
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'TC BE User Admin',
 	'description' => 'A Module for administer BE users more comfortably',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '1.0.5',
+	'version' => '1.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,14 +34,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.6.0-6.99.99',
+			'typo3' => '4.6.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:47:{s:9:"ChangeLog";s:4:"2a35";s:28:"class.tx_tcbeuser_access.php";s:4:"0f30";s:28:"class.tx_tcbeuser_config.php";s:4:"eb72";s:30:"class.tx_tcbeuser_editform.php";s:4:"5e89";s:31:"class.tx_tcbeuser_grouptree.php";s:4:"5d3e";s:27:"class.tx_tcbeuser_hooks.php";s:4:"5b76";s:30:"class.tx_tcbeuser_overview.php";s:4:"f227";s:32:"class.tx_tcbeuser_pwd_wizard.php";s:4:"6ed0";s:32:"class.tx_tcbeuser_recordlist.php";s:4:"0be6";s:21:"ext_conf_template.txt";s:4:"4fb7";s:12:"ext_icon.gif";s:4:"080d";s:17:"ext_localconf.php";s:4:"7e0c";s:14:"ext_tables.php";s:4:"ee65";s:14:"doc/manual.sxw";s:4:"c3ae";s:19:"doc/wizard_form.dat";s:4:"bab3";s:20:"doc/wizard_form.html";s:4:"9121";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"663d";s:22:"mod1/locallang_mod.xml";s:4:"647a";s:19:"mod1/moduleicon.gif";s:4:"68a6";s:14:"mod2/clear.gif";s:4:"cc11";s:13:"mod2/conf.php";s:4:"b8ec";s:14:"mod2/index.php";s:4:"90df";s:18:"mod2/locallang.xml";s:4:"d5e4";s:22:"mod2/locallang_mod.xml";s:4:"ac9e";s:19:"mod2/moduleicon.gif";s:4:"49a3";s:14:"mod2/pwdgen.js";s:4:"6156";s:14:"mod3/clear.gif";s:4:"cc11";s:13:"mod3/conf.php";s:4:"fa59";s:14:"mod3/index.php";s:4:"bc60";s:18:"mod3/locallang.xml";s:4:"10f6";s:22:"mod3/locallang_mod.xml";s:4:"779d";s:19:"mod3/moduleicon.gif";s:4:"9c7c";s:12:"mod4/ajax.js";s:4:"d664";s:14:"mod4/clear.gif";s:4:"cc11";s:13:"mod4/conf.php";s:4:"ec5c";s:14:"mod4/index.php";s:4:"12c4";s:18:"mod4/locallang.xml";s:4:"0ef5";s:22:"mod4/locallang_mod.xml";s:4:"4193";s:19:"mod4/moduleicon.gif";s:4:"080d";s:17:"mod4/prototype.js";s:4:"725c";s:14:"mod5/clear.gif";s:4:"cc11";s:13:"mod5/conf.php";s:4:"e1c2";s:14:"mod5/index.php";s:4:"dbc0";s:18:"mod5/locallang.xml";s:4:"23d0";s:22:"mod5/locallang_mod.xml";s:4:"4bf7";s:19:"mod5/moduleicon.gif";s:4:"a38a";}',
+	'_md5_values_when_last_written' => 'a:49:{s:9:"ChangeLog";s:4:"4a3b";s:28:"class.tx_tcbeuser_access.php";s:4:"0f30";s:35:"class.tx_tcbeuser_compatibility.php";s:4:"4a4c";s:28:"class.tx_tcbeuser_config.php";s:4:"04d1";s:30:"class.tx_tcbeuser_editform.php";s:4:"5e89";s:31:"class.tx_tcbeuser_grouptree.php";s:4:"5d3e";s:27:"class.tx_tcbeuser_hooks.php";s:4:"5b76";s:30:"class.tx_tcbeuser_overview.php";s:4:"63a5";s:32:"class.tx_tcbeuser_pwd_wizard.php";s:4:"6e18";s:32:"class.tx_tcbeuser_recordlist.php";s:4:"f5e3";s:16:"ext_autoload.php";s:4:"4ad2";s:21:"ext_conf_template.txt";s:4:"4fb7";s:12:"ext_icon.gif";s:4:"080d";s:17:"ext_localconf.php";s:4:"7e0c";s:14:"ext_tables.php";s:4:"ee65";s:14:"doc/manual.sxw";s:4:"c3ae";s:19:"doc/wizard_form.dat";s:4:"bab3";s:20:"doc/wizard_form.html";s:4:"9121";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"663d";s:22:"mod1/locallang_mod.xml";s:4:"647a";s:19:"mod1/moduleicon.gif";s:4:"68a6";s:14:"mod2/clear.gif";s:4:"cc11";s:13:"mod2/conf.php";s:4:"b8ec";s:14:"mod2/index.php";s:4:"543a";s:18:"mod2/locallang.xml";s:4:"d5e4";s:22:"mod2/locallang_mod.xml";s:4:"ac9e";s:19:"mod2/moduleicon.gif";s:4:"49a3";s:14:"mod2/pwdgen.js";s:4:"6156";s:14:"mod3/clear.gif";s:4:"cc11";s:13:"mod3/conf.php";s:4:"fa59";s:14:"mod3/index.php";s:4:"92c4";s:18:"mod3/locallang.xml";s:4:"10f6";s:22:"mod3/locallang_mod.xml";s:4:"779d";s:19:"mod3/moduleicon.gif";s:4:"9c7c";s:12:"mod4/ajax.js";s:4:"d664";s:14:"mod4/clear.gif";s:4:"cc11";s:13:"mod4/conf.php";s:4:"ec5c";s:14:"mod4/index.php";s:4:"0018";s:18:"mod4/locallang.xml";s:4:"0ef5";s:22:"mod4/locallang_mod.xml";s:4:"4193";s:19:"mod4/moduleicon.gif";s:4:"080d";s:17:"mod4/prototype.js";s:4:"725c";s:14:"mod5/clear.gif";s:4:"cc11";s:13:"mod5/conf.php";s:4:"e1c2";s:14:"mod5/index.php";s:4:"8341";s:18:"mod5/locallang.xml";s:4:"23d0";s:22:"mod5/locallang_mod.xml";s:4:"4bf7";s:19:"mod5/moduleicon.gif";s:4:"a38a";}',
 	'suggests' => array(
 	),
 );
